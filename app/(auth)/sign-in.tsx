07dd198d64fd13dcb12eb04f22f3e460f9662a17
amirmasoud763 <amirmasoud763@gmail.com>
2026-09-14 13:15:44 +0330

    feat: project setup, nativewind, routing, tab navigation

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,22 @@
+import { Link } from "expo-router";
+import { styled } from "nativewind";
+import React from "react";
+import { Text } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+
+const SafeAreaView = styled(RNSafeAreaView);
+const SignIn = () => {
+  return (
+    <SafeAreaView className="flex-1 p-5 bg-background">
+      <Text>SignIn</Text>
+      <Link
+        href={"/(auth)/sign-up"}
+        className="mt-4 rounded bg-primary text-white p-4"
+      >
+        SignUp
+      </Link>
+    </SafeAreaView>
+  );
+};
+
+export default SignIn;
